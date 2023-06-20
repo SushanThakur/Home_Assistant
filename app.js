2531d07e55c55dbfc6483c5a4018b8c7a98d3dfc
Sushant Thakur <mail@sushant.name.np>
2023-06-20 20:18:57 +0545

    someting

--- app.js
+++ app.js
@@ -1,20 +1,81 @@
-document.getElementById("bulbON").onclick = function () {
-    document.getElementById("tiles_light_id").style.borderColor = "yellow";
-    document.getElementById("tilesLightStatus").style.color = "yellow";
-    document.getElementById("tilesLightStatus").innerHTML = "ON";
-    document.getElementById("bulbOFF").style.backgroundColor = "grey";
-    document.getElementById("bulbON").style.backgroundColor = "#35db3a";  //green color
-    document.getElementById("light_off_png_id").classList.remove("vis");
-    document.getElementById("light_on_png_id").classList.add("vis");
+let tilesLightStatus = document.getElementById("tilesLightStatus");
+let tiles_light = document.getElementById("tiles_light");
+let bulbOFFButton = document.getElementById("bulbOFFButton");
+let bulbONButton = document.getElementById("bulbONButton");
+let light_off_png_id = document.getElementById("light_off_png_id");
+let light_on_png_id = document.getElementById("light_on_png_id");
+
+let tiles_socket = document.getElementById("tiles_socket");
+let socket_on_png = document.getElementById("socket_on_png");
+let socket_off_png = document.getElementById("socket_off_png");
+let powerSocketStatus = document.getElementById("powerSocketStatus");
+let PowerSocketONButton = document.getElementById("PowerSocketONButton");
+let PowerSocketOFFButton = document.getElementById("PowerSocketOFFButton");
+
+
+
+bulbONButton.onclick = function () {
+    on("bulb");
+}
+bulbOFFButton.onclick = function () {
+    off("bulb");
+}
+PowerSocketONButton.onclick = function () {
+    on("socket");
+}
+PowerSocketOFFButton.onclick = function () {
+    off("socket");
+}
+function on(x) {
+    switch (x) {
+        case "bulb":
+            bulbONButton.style.backgroundColor = "#35db3a";  //green color
+            bulbOFFButton.style.backgroundColor = "grey";
+
+            tilesLightStatus.innerHTML = "ON";
+
+            tiles_light.style.borderColor = "yellow";
+            //tilesLightStatus.style.color = "yellow";
+
+            light_off_png.classList.remove("vis");
+            light_on_png.classList.add("vis");
+            break;
+
+        case "socket":
+            PowerSocketONButton.style.backgroundColor = "#35db3a";
+            PowerSocketOFFButton.style.backgroundColor = "grey";
+
+            powerSocketStatus.innerHTML = "ON";
+
+            socket_on_png.classList.add("vis");
+            socket_off_png.classList.remove("vis");
+            break;
+    }
 }
 
-document.getElementById("bulbOFF").onclick = function () {
-    document.getElementById("tiles_light_id").style.borderColor = "aqua";
-    document.getElementById("tilesLightStatus").style.color = "red";
-    document.getElementById("tilesLightStatus").innerHTML = "OFF";
-    document.getElementById("bulbOFF").style.backgroundColor = "#db4035"; //red color
-    document.getElementById("bulbON").style.backgroundColor = "grey";
+function off(x) {
+    switch (x) {
+        case "bulb":
+            bulbONButton.style.backgroundColor = "grey";  //green color
+            bulbOFFButton.style.backgroundColor = "#fa3f39";
 
-    document.getElementById("light_on_png_id").classList.remove("vis");
-    document.getElementById("light_off_png_id").classList.add("vis");
+            tilesLightStatus.innerHTML = "OFF";
+
+            tiles_light.style.borderColor = "RED";
+            //tilesLightStatus.style.color = "RED";
+
+            light_on_png.classList.remove("vis");
+            light_off_png.classList.add("vis");
+            break;
+
+        case "socket":
+            PowerSocketONButton.style.backgroundColor = "grey";
+            PowerSocketOFFButton.style.backgroundColor = "#fa3f39";
+
+            powerSocketStatus.innerHTML = "OFF";
+
+            socket_on_png.classList.remove("vis");
+            socket_off_png.classList.add("vis");
+            break;
+    }
 }
